b80232249cecfd213ff7391f23da13f54c4bacc2
DineshAravind <47448294+DineshAravind@users.noreply.github.com>
2019-02-08 16:52:18 +0530

    Create progam.c

---
+++ progam.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    printf("enter a number:");
+    scanf("%d",&a);
+    if(a>0)
+    {
+        printf("Positive");
+    }
+    else if(a==0)
+    {
+        printf("zero");
+    }
+    else
+    {
+        printf("Negative");
+    }
+        
+    
+}
